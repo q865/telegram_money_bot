721d5544ff2d25427bcab72601f67f7e32b0c2f4
Sergey Panin <men865@gmail.com>
2021-12-29 13:05:27 +0300

    debag 3

--- src/app.js
+++ src/app.js
@@ -40,7 +40,6 @@ var telegraf_1 = require("telegraf");
 var google_spreadsheet_1 = require("google-spreadsheet");
 require('dotenv').config();
 var bot = new telegraf_1.Telegraf(process.env.BOT_TOKEN);
-var creds = require('./creds.json');
 var doc = new google_spreadsheet_1.GoogleSpreadsheet("1qHg3PQmBv0S1ZBGHiOmFHSUPkpIH0aK9Q4Vjlx0-0qw");
 var db = {
     guns: [
@@ -72,72 +71,46 @@ function getRandomInt(num) {
 }
 function start() {
     return __awaiter(this, void 0, void 0, function () {
-        var sheet;
         var _this = this;
         return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, doc.useServiceAccountAuth(creds)];
-                case 1:
-                    _a.sent();
-                    return [4 /*yield*/, doc.loadInfo()];
-                case 2:
-                    _a.sent();
-                    sheet = doc.sheetsByIndex[0];
-                    bot.start(function (ctx) { return ctx.reply('welcome'); });
-                    bot.on('text', function (ctx) { return __awaiter(_this, void 0, void 0, function () {
-                        var rows, idName, sendlerName, msg, r, r2, counter, list, answer, regExp;
-                        var _a, _b, _c, _d, _e;
-                        return __generator(this, function (_f) {
-                            switch (_f.label) {
-                                case 0: return [4 /*yield*/, sheet.getRows()];
-                                case 1:
-                                    rows = _f.sent();
-                                    console.log(rows[0]);
-                                    idName = ctx.message.from.username;
-                                    sendlerName = ctx.message.from.first_name;
-                                    msg = ctx.message.text;
-                                    if (msg === 'двухстволка')
-                                        ctx.reply("".concat(db.guns[getRandomInt(db.guns.length)]));
-                                    if (msg === 'дрочка')
-                                        ctx.reply("".concat(db.drochka[getRandomInt(db.drochka.length)]));
-                                    r = /(?<=(^|[^а-я]))((у|[нз]а|(хитро|не)?вз?[ыьъ]|с[ьъ]|(и|ра)[зс]ъ?|(о[тб]|под)[ьъ]?|(.\B)+?[оаеи])?-?([её]б(?!о[рй])|и[пб][ае][тц]).*?|(н[иеа]|([дп]|верт)о|ра[зс]|з?а|с(ме)?|о(т|дно)?|апч)?-?ху([яйиеёю]|ли(?!ган)).*?|(в[зы]|(три|два|четыре)жды|(н|сук)а)?-?бл(я(?!(х|ш[кн]|мб)[ауеыио]).*?|[еэ][дт]ь?)|(ра[сз]|[зн]а|[со]|вы?|п(ере|р[оие]|од)|и[зс]ъ?|[ао]т)?п[иеё]зд.*?|(за)?п[ие]д[аое]?р(ну.*?|[оа]м|(ас)?(и(ли)?[нщктл]ь?)?|(о(ч[еи])?|ас)?к(ой)|юг)[ауеы]?|манд([ауеыи](л(и[сзщ])?[ауеиы])?|ой|[ао]вошь?(е?к[ауе])?|юк(ов|[ауи])?)|муд([яаио].*?|е?н([ьюия]|ей))|мля([тд]ь)?|лять|([нз]а|по)х|м[ао]л[ао]фь([яию]|[еёо]й))(?=($|[^а-я]))/ui.exec(msg);
-                                    if (r)
-                                        ctx.reply("@".concat(idName, ",\n").concat(sendlerName, ", \u043A\u0430\u0436\u0435\u0442\u0441\u044F \u0432\u044B ").concat(db.insults[getRandomInt(db.insults.length)]));
-                                    r2 = /рулетка.*\d+/.exec(msg);
-                                    if (r2) {
-                                        counter = r2[0].split(' ')[1];
-                                        list = [];
-                                        while (counter) {
-                                            answer = list.push(getRandomInt(100));
-                                            counter -= 1;
-                                        }
-                                        ctx.reply("".concat(list));
-                                    }
-                                    regExp = /(?<date>\w+)\s+(?<type>\w+)\s+(?<category>\w+)\s+(?<amount>\d+)\s+(?<description>\w+)/
-                                        .exec(ctx.message.text);
-                                    if (!(regExp == null)) return [3 /*break*/, 2];
-                                    ctx.reply("Неправильный текст");
-                                    return [3 /*break*/, 4];
-                                case 2: return [4 /*yield*/, sheet.addRow({
-                                        date: (_a = regExp === null || regExp === void 0 ? void 0 : regExp.groups) === null || _a === void 0 ? void 0 : _a.date,
-                                        type: (_b = regExp === null || regExp === void 0 ? void 0 : regExp.groups) === null || _b === void 0 ? void 0 : _b.type,
-                                        category: (_c = regExp === null || regExp === void 0 ? void 0 : regExp.groups) === null || _c === void 0 ? void 0 : _c.category,
-                                        amount: (_d = regExp === null || regExp === void 0 ? void 0 : regExp.groups) === null || _d === void 0 ? void 0 : _d.amount,
-                                        description: (_e = regExp === null || regExp === void 0 ? void 0 : regExp.groups) === null || _e === void 0 ? void 0 : _e.description
-                                    })];
-                                case 3:
-                                    _f.sent();
-                                    _f.label = 4;
-                                case 4: return [2 /*return*/];
-                            }
-                        });
-                    }); });
-                    bot.launch();
-                    // Enable graceful stop
-                    process.once('SIGINT', function () { return bot.stop('SIGINT'); });
-                    process.once('SIGTERM', function () { return bot.stop('SIGTERM'); });
+            // await doc.useServiceAccountAuth({
+            // client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL!,
+            // private_key: process.env.GOOGLE_PRIVATE_KEY!
+            // });
+            // await doc.loadInfo()
+            // const sheet = doc.sheetsByIndex[0]
+            bot.start(function (ctx) { return ctx.reply('welcome'); });
+            bot.on('text', function (ctx) { return __awaiter(_this, void 0, void 0, function () {
+                var idName, sendlerName, msg, r, r2, counter, list, answer;
+                return __generator(this, function (_a) {
+                    idName = ctx.message.from.username;
+                    sendlerName = ctx.message.from.first_name;
+                    msg = ctx.message.text;
+                    if (msg === 'двухстволка')
+                        ctx.replyWithPhoto("".concat(db.guns[getRandomInt(db.guns.length)]));
+                    if (msg === 'дрочка')
+                        ctx.replyWithHTML("<b>".concat(db.drochka[getRandomInt(db.drochka.length)], "</b>"));
+                    r = /(?<=(^|[^а-я]))((у|[нз]а|(хитро|не)?вз?[ыьъ]|с[ьъ]|(и|ра)[зс]ъ?|(о[тб]|под)[ьъ]?|(.\B)+?[оаеи])?-?([её]б(?!о[рй])|и[пб][ае][тц]).*?|(н[иеа]|([дп]|верт)о|ра[зс]|з?а|с(ме)?|о(т|дно)?|апч)?-?ху([яйиеёю]|ли(?!ган)).*?|(в[зы]|(три|два|четыре)жды|(н|сук)а)?-?бл(я(?!(х|ш[кн]|мб)[ауеыио]).*?|[еэ][дт]ь?)|(ра[сз]|[зн]а|[со]|вы?|п(ере|р[оие]|од)|и[зс]ъ?|[ао]т)?п[иеё]зд.*?|(за)?п[ие]д[аое]?р(ну.*?|[оа]м|(ас)?(и(ли)?[нщктл]ь?)?|(о(ч[еи])?|ас)?к(ой)|юг)[ауеы]?|манд([ауеыи](л(и[сзщ])?[ауеиы])?|ой|[ао]вошь?(е?к[ауе])?|юк(ов|[ауи])?)|муд([яаио].*?|е?н([ьюия]|ей))|мля([тд]ь)?|лять|([нз]а|по)х|м[ао]л[ао]фь([яию]|[еёо]й))(?=($|[^а-я]))/ui.exec(msg);
+                    if (r)
+                        ctx.reply("@".concat(idName, ",\n").concat(sendlerName, ", \u043A\u0430\u0436\u0435\u0442\u0441\u044F \u0432\u044B ").concat(db.insults[getRandomInt(db.insults.length)]));
+                    r2 = /рулетка.*\d+/.exec(msg);
+                    if (r2) {
+                        counter = r2[0].split(' ')[1];
+                        list = [];
+                        while (counter) {
+                            answer = list.push(getRandomInt(100));
+                            counter -= 1;
+                        }
+                        ctx.reply("".concat(list));
+                    }
                     return [2 /*return*/];
-            }
+                });
+            }); });
+            bot.launch();
+            // Enable graceful stop
+            process.once('SIGINT', function () { return bot.stop('SIGINT'); });
+            process.once('SIGTERM', function () { return bot.stop('SIGTERM'); });
+            return [2 /*return*/];
         });
     });
 }
