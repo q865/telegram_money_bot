2c73ad99af5a6ece945bd2f9097f8be951e984d1
Sergey Panin <men865@gmail.com>
2022-01-02 19:25:03 +0300

    test 2

--- src/components/rulet.ts
+++ src/components/rulet.ts
@@ -2,7 +2,6 @@ import { getRandomInt } from "./utils";
 
 export default async function (ctx:any) {
     ctx.reply('сколько у тебя фишек?')
-    const response = async () => await ctx.message.text
-    ctx.reply(`${response}`)
+
 }
 
